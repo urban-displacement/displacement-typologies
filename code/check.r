0cd95ff415d48e61fe0d716ab379d9f3b55d8144
Tim Thomas <timthomas@berkeley.edu>
2020-04-08 08:18:32 -0700

    4_noaffordmovers.html

--- code/check.r
+++ code/check.r
@@ -44,7 +44,32 @@ left_join(., memdf %>% group_by(typ_cat) %>% summarise(mem = n()))
 10 []           5     5    35     5
 
 1_noliafford.html
+   typ_cat    atl   chi   den   mem
+   <chr>    <int> <int> <int> <int>
+ 1 ['AdvG']     1    11    NA    NA
+ 2 ['ARE']    207   558   159    73
+ 3 ['ARG']     54   148    36    20
+ 4 ['BE']      16    81    23     1
+ 5 ['EOG']      2    14    NA     1
+ 6 ['OD']      19   115    14    21
+ 7 ['SAE']     21   139    12    20
+ 8 ['SLI']    174   395   157    50
+ 9 ['SMMI']   239   516   239    41
+10 []           5     5    35     5
 2_noliaffordmovers.html
+   typ_cat    atl   chi   den   mem
+   <chr>    <int> <int> <int> <int>
+ 1 ['AdvG']     2    14    NA     2
+ 2 ['ARE']    206   555   159    71
+ 3 ['ARG']     54   148    36    19
+ 4 ['BE']      16    81    23     1
+ 5 ['EOG']      3    24     1     2
+ 6 ['OD']      18   111    13    21
+ 7 ['SAE']     21   139    12    20
+ 8 ['SLI']    174   389   157    50
+ 9 ['SMMI']   239   516   239    41
+10 []           5     5    35     5
+
 3_noliaffordmoversloss.html
 4_noaffordmovers.html
 5_noaffordmoversloss.html
