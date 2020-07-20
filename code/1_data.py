35ea64f495fa674726b3ebc39ae7d8fa4691e265
Tim Thomas <timthomas@berkeley.edu>
2020-07-20 14:13:28 -0700

    adding 2018 shp

--- code/1_data.py
+++ code/1_data.py
@@ -712,24 +712,18 @@ def filter_FIPS(df):
 # ### Creates crosswalking function
 
 def crosswalk_files (df, xwalk, counts, medians, df_fips_base, xwalk_fips_base, xwalk_fips_horizon):
-    # merge dataframe with xwalk file
-    df_merge = df.merge(xwalk[['weight', xwalk_fips_base, xwalk_fips_horizon]], left_on = df_fips_base, right_on = xwalk_fips_base, how='left')                             
+    df_merge = df.merge(xwalk[['weight', xwalk_fips_base, xwalk_fips_horizon]], left_on = df_fips_base, right_on = xwalk_fips_base, how='left') # merge dataframe with xwalk file
     df = df_merge
-    # apply interpolation weight
-    new_var_list = list(counts)+(medians)
+    new_var_list = list(counts)+(medians)     # apply interpolation weight
     for var in new_var_list:
         df[var] = df[var]*df['weight']
-    # aggregate by horizon census tracts fips
-    df = df.groupby(xwalk_fips_horizon).sum().reset_index()
-    # rename trtid10 to FIPS & FIPS to trtid_base
+    df = df.groupby(xwalk_fips_horizon).sum().reset_index() # aggregate by horizon census tracts fips
     df = df.rename(columns = {'FIPS':'trtid_base',
-                              'trtid10':'FIPS'})  
-    # fix state, county and fips code
-    df ['state'] = df['FIPS'].astype('int64').astype(str).str.zfill(11).str[0:2]
+                              'trtid10':'FIPS'})      # rename trtid10 to FIPS & FIPS to trtid_base
+    df ['state'] = df['FIPS'].astype('int64').astype(str).str.zfill(11).str[0:2]     # fix state, county and fips code
     df ['county'] = df['FIPS'].astype('int64').astype(str).str.zfill(11).str[2:5]
-    df ['tract'] = df['FIPS'].astype('int64').astype(str).str.zfill(11).str[5:]
-    # drop weight column
-    df = df.drop(columns = ['weight'])
+    df ['tract'] = df['FIPS'].astype('int64').astype(str).str.zfill(11).str[5:] 
+    df = df.drop(columns = ['weight'])     # drop weight column
     return df
 
 ### Crosswalking
@@ -747,8 +741,6 @@ census_90_xwalked = crosswalk_files (census_90, xwalk_90_10,  counts, medians, d
 
 
 # ###### 2000 Census Data
-
-
 
 counts = census_00.columns.drop(['county', 'state', 'tract', 'mrent_00', 'mhval_00', 'hinc_00', 'FIPS'])
 medians = ['mrent_00', 'mhval_00', 'hinc_00']
