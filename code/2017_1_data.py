9667b2a1ff41d8cba52d7a0f0c3c1e9a6871c3d8
Tim Thomas <timthomas@berkeley.edu>
2020-08-09 17:53:17 -0700

    add home path fix

--- code/2017_1_data.py
+++ code/2017_1_data.py
@@ -17,7 +17,7 @@ import census
 import pandas as pd
 import numpy as np
 import sys
-# import pyarrow.parquet as pq
+from pathlib import Path
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
